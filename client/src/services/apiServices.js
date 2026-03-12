40f2ae882cb18c5fa2b603dc7f2532fb18a1c0a4
PremDev324 <rpremkumar324@gmail.com>
2026-03-12 09:02:39 +0530

    refactor: modularize API calls into dedicated services

---
+++ client/src/services/apiServices.js
@@ -0,0 +1,40 @@
+import API from './api';
+
+export const productService = {
+    getProducts: (keyword = '', page = 1) => 
+        API.get(`/products?keyword=${keyword}&pageNumber=${page}`),
+    
+    getProductById: (id) => 
+        API.get(`/products/${id}`),
+    
+    createProduct: (productData) => 
+        API.post('/products', productData),
+    
+    updateProduct: (id, productData) => 
+        API.put(`/products/${id}`, productData),
+    
+    deleteProduct: (id) => 
+        API.delete(`/products/${id}`),
+};
+
+export const cartService = {
+    getCart: () => 
+        API.get('/cart'),
+    
+    addToCart: (productId, quantity) => 
+        API.post('/cart', { productId, quantity }),
+    
+    removeFromCart: (productId) => 
+        API.delete(`/cart/${productId}`),
+};
+
+export const orderService = {
+    createOrder: (orderData) => 
+        API.post('/orders', orderData),
+    
+    getMyOrders: () => 
+        API.get('/orders/myorders'),
+    
+    getOrderById: (id) => 
+        API.get(`/orders/${id}`),
+};
